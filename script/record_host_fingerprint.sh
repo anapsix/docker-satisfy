d5b22adc68a651a7e731fdd38f7dd525231353b1
Anastas Dancha <anapsix@random.io>
2019-02-14 19:56:56 +0300

    fixing bad git host extract pattern

--- script/record_host_fingerprint.sh
+++ script/record_host_fingerprint.sh
@@ -13,6 +13,6 @@ SATIS_FILE="${APP_ROOT}/satis.json"
 chown ${APP_USER}:${APP_USER} ${APP_USER_HOME}/.ssh/known_hosts
 chmod 600 ${APP_USER_HOME}/.ssh/known_hosts
 
-for host in $(jq -r '.repositories | .[] | select(.type=="git") | .url | scan("(?<=@)[A-Za-z0-9-]+\\.[a-z]+")' ${SATIS_FILE}); do
+for host in $(jq -r '.repositories | .[] | select(.type=="git") | .url | scan("(?<=@)[A-Za-z0-9-]+[^:]+")' ${SATIS_FILE}); do
   ssh-keyscan $host >> ${APP_USER_HOME}/.ssh/known_hosts
 done
